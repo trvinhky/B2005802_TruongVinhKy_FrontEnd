da6653ffd93b5b3319f345d0bcb6ba96aeebbd11
trvinhky <vinhky26032002@gmail.com>
2024-03-19 21:29:52 +0700

    Tao them trang them moi lien he

---
+++ src/views/ContactCreate.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="page">
+        <h4>Hiệu chỉnh Liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="createContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {
+                name: '',
+                email: '',
+                address: '',
+                phone: '',
+                favorite: false
+            },
+            message: "",
+        };
+    },
+    methods: {
+        async createContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ đã được thêm mới thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    },
+    created() {
+        this.message = "";
+    },
+}
+</script>
